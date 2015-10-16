14583a023fdff386bf4031dc79dadeb4cd9e4146
NHashikin86 <hashikin.abdul@gmail.com>
2015-10-16 17:54:02 +0800

    new commit

--- src/test2companyb/ThreadsFeature.java
+++ src/test2companyb/ThreadsFeature.java
@@ -3,10 +3,14 @@ package test2companyb;
 
 public class ThreadsFeature{
     public static void main (String[] args){
-               ThreadsFeature sample=new ThreadsFeature();
-               sample.start();
-               try{Thread.sleep(500);}
-               catch(Exception ex){}
-               sample.finished=true;
+        CountUp up = new CountUp();
+        CountDown down= new CountDown();
+        up.start();
+        down.start();
+        try{Thread.sleep(5000);}//**break for 5s
+        catch(InterruptedException ex){}
+        up.finished=true;
+        down.finished=true;
+        
            }
 }
