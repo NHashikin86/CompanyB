26530177f9cf5968a9321dc4b96459b22677af49
NHashikin86 <hashikin.abdul@gmail.com>
2015-10-16 17:23:09 +0800

    new commit

---
+++ src/test2companyb/CountUp.java
@@ -0,0 +1,14 @@
+//** Try create for multithreads
+
+package test2companyb;
+
+public class CountUp extends Thread{
+    public boolean finished=false;
+    public void run();{
+    for(int x=1;x<10000000;x++){
+        if (finished) break;
+        System.out.println("CountUp : "+x);
+    }
+}
+    
+}
