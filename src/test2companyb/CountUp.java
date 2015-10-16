29006a74e688968892561df13bf27470d669f54e
NHashikin86 <hashikin.abdul@gmail.com>
2015-10-16 17:53:44 +0800

    new commit

--- src/test2companyb/CountUp.java
+++ src/test2companyb/CountUp.java
@@ -3,11 +3,13 @@
 package test2companyb;
 
 public class CountUp extends Thread{
+   
     public boolean finished=false;
-    public void run();{
+   
+        public void run(){
     for(int x=1;x<10000000;x++){
         if (finished) break;
-        System.out.println("CountUp : "+x);
+        System.out.println("CountUp : ");
     }
 }
     
