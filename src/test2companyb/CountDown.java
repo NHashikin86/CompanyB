575314ce17ab92b297c1a43b774f0369c3ec8993
NHashikin86 <hashikin.abdul@gmail.com>
2015-10-16 17:52:09 +0800

    new commit

--- src/test2companyb/CountDown.java
+++ src/test2companyb/CountDown.java
@@ -4,7 +4,7 @@ package test2companyb;
 
 public class CountDown extends Thread{
     public boolean finished=false;
-    public void run();{
+    public void run(){
     for(int x=10000000;x>0;x--){
         if (finished) break;
         System.out.println("CountDown : "+x);
